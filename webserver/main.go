16ba06653ba169a4f8e50c785435f134cf437e74
tanyichao <tanyichao110@163.com>
2020-10-12 16:50:30 +0800

    3

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> Hello,I am vanman2</h1>")
+	io.WriteString(w, "<h1> Hello,I am vanman3</h1>")
 }
 func main() {
 	http.HandleFunc("/", firstPage)
