56515284d0fe711f5c156ab0fd3744a09e6639a1
tanyichao <tanyichao110@163.com>
2020-10-12 11:56:20 +0800

    change webserver page

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> this is my firstPage</h1>")
+	io.WriteString(w, "<h1> Hello,I am vanman</h1>")
 }
 func main() {
 	http.HandleFunc("/", firstPage)
