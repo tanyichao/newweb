c3e8b8360e84895d280c2129403ba7d6af9963a0
tanyichao <tanyichao110@163.com>
2020-10-13 09:41:04 +0800

    fix os bug

--- webserver/main.go
+++ webserver/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func firstPage(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "<h1> Hello,I am vanman5</h1>")
+	io.WriteString(w, "<h1> Hello,I am vanman6</h1>")
 }
 func main() {
 	http.HandleFunc("/", firstPage)
